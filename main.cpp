e77e125e79688d536f7b47a7bb0de705b74c0dde
김민서 <xszc0123@naver.com>
2025-04-05 00:18:50 +0900

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+ 
+int main(void)
+{
+	/*
+	첫 번째 작성한 C 프로그램입니다.
+	2025.04.05
+	*/
+	printf("Hello ! \n\n");
+	printf("I am a Student. \n");
+	printf("%d\n", 1234);
+	printf("%d %d\n", 10, 20);
+	return 1;
+
+}
+
+// 한 줄 주석
+
